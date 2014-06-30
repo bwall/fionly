76d4ebf28bc70eec6caea37e982c6445f74bfa0e
bwall <bwall@openbwall.com>
2014-06-30 00:28:01 -0700

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='fionly',
+      version='1.0.0',
+      description='Prints out the first unique instance of a line',
+      author='Brian Wallace',
+      author_email='bwall@ballastsecurity.net',
+      url='https://github.com/bwall/fionly',
+      packages=[],
+      scripts=['fionly'],
+     )
